ebc1d8fa8227cb58cd92343fe138e9e377a22040
bbwwgg <jackedixon4@gmail.com>
2025-11-03 02:47:42 +1100

    Feat: added verifyBasicUser

--- src/middleware/UserCRUDValidation.js
+++ src/middleware/UserCRUDValidation.js
@@ -1,4 +1,58 @@
 const { UserModel } = require("../database/entities/User");
+const { generateJWT, validateJWT} = require("./jwtFunctions");
+
+
+//Confirms that a given username and password are correct
+async function verifyBasicUserAuth (request, response, next) {
+  
+  //Get the authorization header from an request
+  let authHeader = request.headers["authorization"] ?? null;
+
+  //if no header is provided, exit
+  if (authHeader == null){
+    return next(new Error("No auth data given"));_
+  }
+  
+  //Check its a basic auth string
+  if (authHeader.startsWith("Basic ")) {
+    authHeader = authHeader.substring(5).trim();
+  }
+
+  //Decode the auth
+  let decodedAuth = Buffer.from(authHeader, 'base64').toString('ascii');
+
+  // Convert it into a usable object.
+  let objDecodedAuth = {email: '', password: ''};
+  objDecodedAuth.email = decodedAuth.substring(0, decodedAuth.indexOf(":"));
+  objDecodedAuth.password = decodedAuth.substring(decodedAuth.indexOf(":") + 1);
+
+	// Check if a user exists for the given login email.
+	let foundUser = await UserModel.findOne({email: objDecodedAuth.email});
+
+	if (!foundUser || foundUser == null){
+		return next(new Error("No user found for the given auth data."));
+	}
+
+  //Check if the password matches the found user
+	let doesPasswordMatch = await foundUser.isMatchingPassword(objDecodedAuth.password);
+	if (!doesPasswordMatch){
+    //Do not let the error know that the password is incorrect
+		return next(new Error("No user matches the given auth data."));
+	}
+
+  //Set up extra data for other middleware
+	request.authentication = {
+		...request.authentication,
+		id: foundUser.id,
+		user: foundUser
+	}
+
+
+  next();
+
+}
+
+
 
 
 async function validateRegisterData (request, response, next) {
