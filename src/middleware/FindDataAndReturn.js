c598847a7267f38478ef4df684079997de5995de
Phuong Le <82745747+phuongle1911@users.noreply.github.com>
2025-11-03 21:45:56 +1100

    fix: deleted a file

--- src/middleware/FindDataAndReturn.js
+++
@@ -1,8 +0,0 @@
-function verifyFindDataAndReturn(data, name){
-  if (data) {
-    response.json(data)
-  } else {
-    return next(new Error(`${name} not found!`))
-  }
-};
-module.exports = { verifyFindDataAndReturn }
