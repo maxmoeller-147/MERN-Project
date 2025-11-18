70789f008bf129a29ab4a300915911c66c9f8613
Max Moeller <maximomoeller@gmail.com>
2025-11-18 18:21:50 +1100

    feat: FriendList Test

---
+++ Frontend/message_app_project/src/__tests__/FriendsList.test.jsx
@@ -0,0 +1,18 @@
+import { FriendList } from "../components/FriendsList";
+import { describe, expect, test, vi } from "vitest";
+import { render, screen } from "@testing-library/react"
+import { FriendUser } from "../components/FriendUser";
+
+vi.mock("../components/FriendList", () => ({
+    FriendUser: ({ friend }) => (
+        <div data-testid="friend-user">{friend.name}</div>
+    )
+}));
+
+describe("FriendList", () => {
+    test("Show message if it's empty", () => {
+        render(<FriendList friends={[]} currentUserId="1"/>);
+        expect(screen.getByText(/you have no friends yet/i)).toBeInTheDocument();
+    });
+});
+
