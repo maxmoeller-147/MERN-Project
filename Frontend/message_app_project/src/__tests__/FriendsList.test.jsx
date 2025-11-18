7dd927c53bdc71b0c18dd6a03c8614972ee45678
Max Moeller <maximomoeller@gmail.com>
2025-11-18 20:38:49 +1100

    feat: test logout button

--- Frontend/message_app_project/src/__tests__/FriendsList.test.jsx
+++ Frontend/message_app_project/src/__tests__/FriendsList.test.jsx
@@ -4,15 +4,15 @@ import { render, screen } from "@testing-library/react"
 import { FriendUser } from "../components/FriendUser";
 
 vi.mock("../components/FriendList", () => ({
-    FriendUser: ({ friend }) => (
-        <div data-testid="friend-user">{friend.name}</div>
-    )
+  FriendUser: ({ friend }) => (
+    <div data-testid="friend-user">{friend.name}</div>
+  )
 }));
 
 describe("FriendList", () => {
-    test("Show message if it's empty", () => {
-        render(<FriendList friends={[]} currentUserId="1"/>);
-        expect(screen.getByText(/you have no friends yet/i)).toBeInTheDocument();
-    });
+  test("Show message if it's empty", () => {
+    render(<FriendList friends={[]} currentUserId="1"/>);
+    expect(screen.getByText(/you have no friends yet/i)).toBeInTheDocument();
+  });
 });
 
---
+++ Frontend/message_app_project/src/__tests__/LogOutBttm.test.jsx
@@ -0,0 +1,54 @@
+import { LogoutButton } from "../components/LogoutBttm";
+import { describe, expect, test, vi } from "vitest";
+import api from "../api";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { MemoryRouter, useNavigate } from "react-router";
+import { use } from "react";
+
+
+const mockNavigate = vi.fn();
+
+
+vi.mock("react-router", async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+  ...actual, 
+  useNavigate: () => mockNavigate,
+  };
+});
+
+
+global.alert = vi.fn();
+
+
+vi.mock("../api", () => ({
+  default: {
+    post: vi.fn()
+  }
+}));
+
+
+describe("Logout Button", () => {
+  test(" Alert, Api and navigate when logout ", async () => {
+    api.post.mockResolvedValue({
+      data: { message:"Logout successfully" }
+    });
+
+    render(
+      <MemoryRouter><LogoutButton/></MemoryRouter>
+    );
+
+    const button = screen.getByRole("button", { name: /log out/i });
+
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(api.post).toHaveBeenCalledWith("/users/logout", {});
+      expect(mockNavigate).toHaveBeenCalledWith("/");
+      expect(alert).toHaveBeenCalledWith("logged out succesfully");
+    });
+  });
+
+
+  
+});
