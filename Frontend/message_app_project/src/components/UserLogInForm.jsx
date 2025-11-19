fce63a63d7a24b513666b978bdf2b774d1cb82ae
Max Moeller <maximomoeller@gmail.com>
2025-11-20 03:24:32 +1100

    commented code affecting css

--- Frontend/message_app_project/src/components/UserLogInForm.jsx
+++ Frontend/message_app_project/src/components/UserLogInForm.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
-import "../styles/LogInPage.css"
+// import "../styles/LogInPage.css"
 
 
 export default function UserLogInForm({ setError }) {
