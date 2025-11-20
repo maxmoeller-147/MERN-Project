a38eae3633bd317138774ec75b56527d314c8fa6
bbwwgg <jackedixon4@gmail.com>
2025-11-21 02:52:02 +1100

    Feat: added nav and buttons to the homepage

--- Frontend/message_app_project/src/components/RoomList.jsx
+++ Frontend/message_app_project/src/components/RoomList.jsx
@@ -1,18 +1,28 @@
 import { useContext, useState } from 'react';
 import { RoomDataContext } from "../Contexts/RoomDataContext";
+import { useNavigate } from "react-router-dom";
 
 export function RoomList() {
   const roomsContext = useContext(RoomDataContext);
   
   const rooms = roomsContext?.rooms || [];
-   
+  const navigate = useNavigate();
+  
+  const onSubmit = (_id) =>{
+    navigate(`/rooms/${_id}`);
+  }
+  
+  
   if (rooms?.length === 0) return <p>Sorry, you arent in any groups</p>;
 
   return (
     <section className='rooms-list'>
       {
         rooms.map(room => (
-          <p key={room._id}> { room.name }</p>
+          <div>
+            <p key={room._id}> { room.name }</p>
+            <button onClick={() => onSubmit(room._id)}>Go to room</button>
+          </div>
         ))
       }
     </section>
